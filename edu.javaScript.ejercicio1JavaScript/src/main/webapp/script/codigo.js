480106dbe2a1efcffb0dc74653a8b53574ebfabf
fragalado <frandanigd@gmail.com>
2023-02-13 10:16:15 +0100

    ad

---
+++ edu.javaScript.ejercicio1JavaScript/src/main/webapp/script/codigo.js
@@ -0,0 +1,10 @@
+/**
+ * CODIGO JAVASCRIPT
+ */
+
+// Mostramos una alerta para que se tenga activado el soporte de javascript
+alert("Comprueba que tengas activado el soporte de JavaScript!");
+// Imprimimos por consola el siguiente texto
+console.log("Hola Mundo!");
+
+document.write("Soy el primer script");
