85078b0222fc1db84084a800075eee6040dc4741
FRAN\frand <frandanigd@gmail.com>
2023-02-13 11:51:48 +0100

    13/02/2023

--- edu.javaScript.ejercicio1JavaScript/src/main/webapp/script/codigo.js
+++ edu.javaScript.ejercicio1JavaScript/src/main/webapp/script/codigo.js
@@ -6,5 +6,5 @@
 alert("Comprueba que tengas activado el soporte de JavaScript!");
 // Imprimimos por consola el siguiente texto
 console.log("Hola Mundo!");
-
+// Escribimos un mensaje en el documento
 document.write("Soy el primer script");
